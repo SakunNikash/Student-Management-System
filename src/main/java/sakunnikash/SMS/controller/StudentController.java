a870a011b737a30a19cf12554277310245a7135d
SakunNikash <sakunnikash@gmail.com>
2021-09-04 22:05:58 +0530

    configure database

--- src/main/java/sakunnikash/SMS/controller/StudentController.java
+++ src/main/java/sakunnikash/SMS/controller/StudentController.java
@@ -1,7 +1,21 @@
 package sakunnikash.SMS.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import sakunnikash.SMS.service.StudentService;
 
 @Controller
 public class StudentController {
+    private StudentService StudentService;
+
+    public StudentController(StudentService studentService) {
+        StudentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        model.addAttribute("students", StudentService.getAllStudents());
+        return  "students";
+    }
 }
